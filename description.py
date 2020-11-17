f2bc36dd04f101a2a1d2cb74d32c7d3f67a23262
Venkat Ragavan <mail2venkat21@gmail.com>
2020-11-18 03:02:35 +0530

    project wrap up

--- description.py
+++ description.py
@@ -2,7 +2,7 @@ desc = [
     [
         {
             "title": "HalfCut Black tees",
-            "price": "Rs. 749"
+            "price": "Rs. 899"
         },
         {
             "title": "Adidas Butterfly tees",
@@ -10,157 +10,103 @@ desc = [
         },
         {
             "title": "Peach Designer tees",
-            "price": "Rs. 749"
+            "price": "Rs. 649"
         },
         {
             "title": "Black Tanks",
-            "price": "Rs. 749"
+            "price": "Rs. 799"
         }
     ],
     [
         {
-            "title": "HalfCut Black tees",
+            "title": "Men's black gym fit tees",
+            "price": "Rs. 649"
+        },
+        {
+            "title": "GANT unisex tees",
             "price": "Rs. 749"
         },
         {
-            "title": "Adidas Butterfly tees",
+            "title": "classic round neck tees",
+            "price": "Rs. 599"
+        },
+        {
+            "title": "white crew-neck tshirt",
+            "price": "Rs. 549"
+        },
+        {
+            "title": "Stone washed tees",
+            "price": "Rs. 699"
+        },
+        {
+            "title": "Butterfly tees",
             "price": "Rs. 749"
         },
         {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
+            "title": "Spykar black polo t-shirt",
+            "price": "Rs. 799"
         },
         {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
+            "title": "Printed t-shirt",
+            "price": "Rs. 699"
         },
         {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
+            "title": "Flying Machine Striped t-shirt",
+            "price": "Rs. 799"
         },
         {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
+            "title": "Nike signature branding t-shirt",
+            "price": "Rs. 899"
         },
         {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
+            "title": "Reebok classic t-shirt black",
+            "price": "Rs. 849"
         },
         {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
+            "title": "Levi's crew neck t-shirt with branding",
+            "price": "Rs. 899"
         },
         {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
+            "title": "Striped hem typographic t-shirt",
+            "price": "Rs. 849"
         }
     ],
     [
         {
-            "title": "HalfCut Black tees",
+            "title": "Net full length sleeves top",
             "price": "Rs. 749"
+        },
+        {
+            "title": "Aurelia deep neck top",
+            "price": "Rs. 699"
+        },
+        {
+            "title": "black wide neck top",
+            "price": "Rs. 749"
+        },
+        {
+            "title": "Black striped tank top",
+            "price": "Rs. 699"
+        },
+        {
+            "title": "Adidas women's swimsuit",
+            "price": "Rs. 1099"
         },
         {
             "title": "Adidas Butterfly tees",
+            "price": "Rs. 949"
+        },
+        {
+            "title": "Full sleeved striped top",
+            "price": "Rs. 899"
+        },
+        {
+            "title": "Lace up netted top",
             "price": "Rs. 749"
         },
         {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        }
-    ],
-    [
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
+            "title": "Reebok pink crop top",
+            "price": "Rs. 899"
         },
         {
             "title": "Adidas Butterfly tees",
@@ -181,40 +127,40 @@ desc = [
     ],
     [
         {
-            "title": "HalfCut Black tees",
+            "title": "UCB yellow t-shirt",
+            "price": "Rs. 849"
+        },
+        {
+            "title": "Frendz striped t-shirt",
+            "price": "Rs. 699"
+        },
+        {
+            "title": "Nike sports tank top",
+            "price": "Rs. 949"
+        },
+        {
+            "title": "US POLO ASSN. full sleeved tees",
+            "price": "Rs. 999"
+        },
+        {
+            "title": "Classic red t-shirt",
+            "price": "Rs. 599"
+        },
+        {
+            "title": "Converse white t-shirt with branding",
+            "price": "Rs. 899"
+        },
+        {
+            "title": "Black typographic print t-shirt",
             "price": "Rs. 749"
         },
         {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
+            "title": "Black round neck tshirt",
+            "price": "Rs. 549"
         },
         {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
+            "title": "ELLESSE round neck black tshirt",
+            "price": "Rs. 849"
         },
         {
             "title": "Adidas Butterfly tees",
@@ -236,97 +182,43 @@ desc = [
     [
         {
             "title": "HalfCut Black tees",
+            "price": "Rs. 849"
+        },
+        {
+            "title": "Full sleeve deep neck top",
+            "price": "Rs. 649"
+        },
+        {
+            "title": "Adidas blocked crop top",
+            "price": "Rs. 949"
+        },
+        {
+            "title": "Women's black sleeveless top",
             "price": "Rs. 749"
         },
         {
-            "title": "Adidas Butterfly tees",
+            "title": "Stone washed wide neck top",
+            "price": "Rs. 799"
+        },
+        {
+            "title": "Reebok Peach fleeted top",
+            "price": "Rs. 949"
+        },
+        {
+            "title": "Women's sleeveless printed top",
             "price": "Rs. 749"
         },
         {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
+            "title": "Aurelia mesh sleeved top",
+            "price": "Rs. 899"
         },
         {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
+            "title": "Pepe jeans back laced top",
+            "price": "Rs. 949"
         },
         {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        }
-    ],
-    [
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
+            "title": "H&M laced crop top",
+            "price": "Rs. 999"
         },
         {
             "title": "Peach Designer tees",
@@ -343,164 +235,160 @@ desc = [
     ],
     [
         {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
+            "title": "Campa Black Sweatshirt",
+            "price": "Rs. 899"
         },
         {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
+            "title": "GAP white full sleaves",
+            "price": "Rs. 699"
         },
         {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
+            "title": "Grey Round Neck T-Shirt",
+            "price": "Rs. 449"
         },
         {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
+            "title": "Grey Tanks",
+            "price": "Rs. 799"
         }
     ],
     [
         {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
+            "title": "Women A-line Orange dress",
+            "price": "Rs. 769"
         },
         {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
+            "title": "Blue checked T-shirt",
+            "price": "Rs. 599"
         },
         {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
+            "title": "Printed Round neck T-shirt",
+            "price": "Rs. 499"
         }
     ],
     [
         {
-            "title": "HalfCut Black tees",
+            "title": "Women blue tees",
+            "price": "Rs. 449"
+        },
+        {
+            "title": "Party shoulder tops",
+            "price": "Rs. 649"
+        },
+        {
+            "title": "PUMA short tops",
+            "price": "Rs. 899"
+        },
+        {
+            "title": "Half cut black tees",
             "price": "Rs. 749"
         },
         {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
+            "title": "Black Sleeveless tops",
+            "price": "Rs. 649"
         },
         {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
+            "title": "Printed women tunic",
+            "price": "Rs. 549"
+        }
+    ],
+    [
+        {
+            "title": "Grey camo T-shirt",
+            "price": "Rs. 549"
         },
         {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Peach Designer tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Black Tanks",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "HalfCut Black tees",
-            "price": "Rs. 749"
-        },
-        {
-            "title": "Adidas Butterfly tees",
-            "price": "Rs. 749"
+            "title": "Black full sleeves",
+            "price": "Rs. 649"
         },
         {
             "title": "Peach Designer tees",
+            "price": "Rs. 599"
+        },
+        {
+            "title": "Graphic printed tees",
+            "price": "Rs. 449"
+        },
+        {
+            "title": "Flea Round Neck T-shirt",
+            "price": "Rs. 649"
+        },
+        {
+            "title": "Levi's blue T-shirt",
             "price": "Rs. 749"
         },
         {
-            "title": "Black Tanks",
+            "title": "Adidas orange tees",
+            "price": "Rs. 1099"
+        },
+        {
+            "title": "Adidas sporty T-shirt",
+            "price": "Rs. 1599"
+        },
+        {
+            "title": "White printed T-shirt",
+            "price": "Rs. 449"
+        },
+        {
+            "title": "U.S.Polo Assn. T-shirt",
+            "price": "Rs. 999"
+        },
+        {
+            "title": "Blue Designer tees",
+            "price": "Rs. 549"
+        },
+        {
+            "title": "White Designer T-shirt",
+            "price": "Rs. 449"
+        },
+        {
+            "title": "Adidas black Tanks",
+            "price": "Rs. 1299"
+        }
+    ],
+    [
+        {
+            "title": "Black Butterfly tees",
+            "price": "Rs. 649"
+        },
+        {
+            "title": "Black Party wear",
+            "price": "Rs. 849"
+        },
+        {
+            "title": "Peach Designer tees",
+            "price": "Rs. 669"
+        },
+        {
+            "title": "Red Designer tees",
+            "price": "Rs. 649"
+        },
+        {
+            "title": "Red Designer top",
+            "price": "Rs. 649"
+        },
+        {
+            "title": "Yellow Butterfly top",
             "price": "Rs. 749"
         },
         {
-            "title": "Black Tanks",
+            "title": "Maroon Nighty wear",
+            "price": "Rs. 629"
+        },
+        {
+            "title": "Red V-neck top",
+            "price": "Rs. 699"
+        },
+        {
+            "title": "Grey tees",
             "price": "Rs. 749"
+        },
+        {
+            "title": "Black sporty top",
+            "price": "Rs. 749"
+        },
+        {
+            "title": "Pink Party wear",
+            "price": "Rs. 1499"
         }
     ]
 ]
